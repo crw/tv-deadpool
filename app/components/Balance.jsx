e298ba22c6f3f20c31c97495cd0c75e1edf4d2f7
Craig Wright <craig@wright.name>
2016-06-07 21:45:21 -0700

    Visual display of username and edit form, first draft.

--- app/components/Balance.jsx
+++ app/components/Balance.jsx
@@ -1,23 +1,89 @@
 import React from 'react';
 import {connect} from 'react-redux';
+// App imports
+import {getKey} from 'app/utils';
+import {DEFAULT_DISPLAY_NAME} from 'app/constants/strings';
 
 
 export class Balance extends React.Component {
   static propTypes = {
-    balance: React.PropTypes.number.isRequired
+    balance: React.PropTypes.number,
+    displayName: React.PropTypes.string
   };
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      editingName: false
+    };
+
+    this.handleClickEditName = this.handleClickEditName.bind(this);
+    this.handleClickCancelEditName = this.handleClickCancelEditName.bind(this);
+  }
+
+  componentDidUpdate() {
+    let {editingName} = this.state;
+    let {editname} = this.refs;
+
+    if (editingName) {
+      editname.focus();
+    }
+  }
+
+  handleClickEditName(e) {
+    e.preventDefault();
+    this.setState({
+      editingName: true
+    });
+  }
+
+  handleClickCancelEditName(e) {
+    e.preventDefault();
+    let {editname} = this.refs;
+    editname.value = '';
+    this.setState({
+      editingName: false
+    });
+  }
+
+  handleSubmit() {
+    return ;
   }
 
   render() {
-    var {balance} = this.props;
+    let {balance, displayName} = this.props;
+    let {editingName} = this.state;
+    let origDisplayName = displayName;
+
+    displayName = displayName || DEFAULT_DISPLAY_NAME;
+    balance = balance || 0;
+
+    let renderDisplayName = () => {
+      if (!editingName) {
+        return (
+          <div className="player-display-name">
+            <i className="fa fa-fw fa-user"/>
+            <span onClick={this.handleClickEditName} className="display-name">{displayName}</span>
+          </div>
+        );
+      } else {
+        let value = origDisplayName || '';
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" ref="editname" placeholder="Change your name." value={value}/>
+            <button className="button">Update</button>
+            <button className="button" onClick={this.handleClickCancelEditName}>Cancel</button>
+          </form>
+        );
+      }
+    };
 
     return (
       <div className="player-balance container">
+        {renderDisplayName()}
         <div className="balance-label">
-          Balance:
+          Balance
         </div>
         <div className="balance-value">
           ${balance.toLocaleString({currency: 'USD'})}
@@ -28,8 +94,8 @@ export class Balance extends React.Component {
 }
 
 export default connect((state) => {
-  let user = (state.login && state.login.user) || null;
+  let user = getKey(state, 'login.user', null);
   return {
-    balance: (user && user.balance) || 0
+    ...user
   };
 })(Balance);
--- app/components/Bet.jsx
+++ app/components/Bet.jsx
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 // App imports
-import {now} from 'app/utils';
+import {now, getKey} from 'app/utils';
 import Wager from 'Wager';
 import WagerForm from 'WagerForm';
 
