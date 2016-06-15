c1d00f708f975c2be3473b2d770e857330148aef
Craig Wright <craig@wright.name>
2016-06-15 11:22:55 -0700

    Resolver for s6e8.

--- scripts/resolve-event.jsx
+++ scripts/resolve-event.jsx
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 // App imports
-import {DEFAULT_DISPLAY_NAME as CONST_DEFAULT_DISPLAY_NAME} from '../app/constants/strings';
-import {isObject, sortObjectsByKey, getKey} from '../app/utils';
+import {DEFAULT_DISPLAY_NAME as DEFAULT_DISPLAY_NAME} from '../app/constants/strings';
+import {getKey} from '../app/utils';
 
 // Initialize the app with a custom auth variable, limiting the server's access
 var config = {
@@ -16,53 +16,55 @@ var config = {
 };
 firebase.initializeApp(config);
 
-const INITIAL_BALANCE = 100;
-const DEFAULT_DISPLAY_NAME = CONST_DEFAULT_DISPLAY_NAME;
-
 console.log('Updating Firebase database', process.env.FIREBASE_DATABASE_URL);
 
-function toArray(firebaseArray) {
-  if (isObject(firebaseArray)) {
-    let itemsArr = [];
-    Object.keys(firebaseArray).forEach((item) => {
-      itemsArr.push(firebaseArray[item])
-      // Preserve the key, which is often the ID for the object.
-      itemsArr[itemsArr.length-1].key = item;
-    });
-    return itemsArr;
-  } else {
-    throw 'Invalid data, not an object.';
-  }
-}
 
+//// CHANGE THIS!!!
+////
+let eventId = 'gameofthrones-6-8';
 
 
 // The app only has access as defined in the Security Rules
 var db = firebase.database();
 var ref = db.ref();
 
+let errorFunc = (err) => {
+  console.log(err);
+};
 
-let eventId = 'gameofthrones-6-7';
+try {
+  ref.child(`events/${eventId}`).once('value').then((snapshot) => {
 
-ref.child(`events/${eventId}`).once('value').then((snapshot) => {
 
-  let event = snapshot.val();
-  let bets = getKey(event, 'bets', {});
+    console.log('Updating event ' + eventId);
 
-  let updateData = {};
+    let event = snapshot.val();
+    let bets = getKey(event, 'bets', {});
 
-  updateData[`events/${eventId}/resolved`] = true;
-  for (let betId of Object.keys(bets)) {
-    updateData[`bets/${betId}/resolved`] = true;
-  }
+    let updateData = {};
 
-  ref.update(updateData).then((snapshot) => {
-      process.exit(0);
-  },(e) => {
-    console.log('Updated error:', e)
-  });
+    updateData[`events/${eventId}/resolved`] = true;
+    for (let betId of Object.keys(bets)) {
+      updateData[`bets/${betId}/resolved`] = true;
+    }
 
-}, (e) => {
-  console.log('error', e);
-});
+    // Any of the Tullys
+    updateData['bets/gameofthrones-6-8-3/paid'] = true;
+    // The Waif
+    updateData['bets/gameofthrones-6-8-6/paid'] = true;
+    // The Under (5.5)
+    updateData['bets/gameofthrones-6-8-17/paid'] = true;
+    // updateData['bets//paid'] = true;
 
+    console.log(updateData);
+
+    ref.update(updateData).then((snapshot) => {
+        console.log(snapshot.val());
+        // process.exit(0);
+    }, errorFunc);
+
+  }, errorFunc);
+
+} catch(err) {
+  errorFunc(err);
+}
