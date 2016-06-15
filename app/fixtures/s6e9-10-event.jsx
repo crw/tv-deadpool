b198295e44367fb8734c5e52e69ee420de29dc94
Craig Wright <craig@wright.name>
2016-06-15 11:23:09 -0700

    Playing around with the fixtures.

---
+++ app/fixtures/s6e9-10-event.jsx
@@ -0,0 +1,25 @@
+import moment from 'moment';
+
+export const Events = [
+  {
+    id: '8',
+    type: 'TV_EPISODE',
+    season: 6,
+    episode: 8,
+    order: 8,
+    series: 'Game of Thrones',
+    name: 'No One',
+    air_at: moment('2016-06-13T01:00:00Z').unix()*1000,
+    lock_at: moment('2016-06-13T00:50:00Z').unix()*1000,
+    article: '',
+    confirmation: false,
+    preview: 'https://youtu.be/fIOiAfEIk_g',
+    published: false,
+    resolved: false,
+    created_at: Date.now(),
+    updated_at: Date.now()
+  }
+];
+
+
+export default Events;
