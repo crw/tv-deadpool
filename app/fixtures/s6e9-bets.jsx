feb14cb2e4776e64c2374ee25d39e4a65fa67a58
Craig Wright <craig@wright.name>
2016-06-16 18:59:29 -0700

    Updated fixtures for ep9

---
+++ app/fixtures/s6e9-bets.jsx
@@ -0,0 +1,144 @@
+import moment from 'moment';
+
+let i = 0;
+
+export const Bets = {
+  '9': [
+    {
+      name: 'Ramsay Bolton',
+      desc: '',
+      odds_payout: 2,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Tormund',
+      desc: '',
+      odds_payout: 3,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Rickon Stark',
+      desc: '',
+      odds_payout: 4,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Ser Davos Seaworth',
+      desc: '',
+      odds_payout: 5,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'The Wildling Giant',
+      desc: 'Wun Weg Wun Dar Wun',
+      odds_payout: 5,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Podrick Payne',
+      desc: '',
+      odds_payout: 8,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'The Hound',
+      desc: '',
+      odds_payout: 8,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Brienne Of Tarth',
+      desc: '',
+      odds_payout: 10,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Any member of The Brotherhood Without Banners',
+      desc: '',
+      odds_payout: 10,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Benjen Stark',
+      desc: '',
+      odds_payout: 10,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Littlefinger',
+      desc: '',
+      odds_payout: 15,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Ghost',
+      desc: '',
+      odds_payout: 15,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Melisandre',
+      desc: '',
+      odds_payout: 15,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Smalljon Umber',
+      desc: '',
+      odds_payout: 20,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Robin of the Vale',
+      desc: '',
+      odds_payout: 20,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Sansa Stark',
+      desc: '',
+      odds_payout: 25,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Meera',
+      desc: '',
+      odds_payout: 25,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Bran',
+      desc: '',
+      odds_payout: 30,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Jon Snow',
+      desc: '',
+      odds_payout: 50,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'No one on this list',
+      desc: '',
+      odds_payout: 100,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Over on total deaths: 5.5',
+      desc: '',
+      odds_payout: 1,
+      odds_wager: 1,
+      order: ++i,
+    }, {
+      name: 'Under on total deaths: 5.5',
+      desc: '',
+      odds_payout: 1,
+      odds_wager: 1,
+      order: ++i,
+    }
+  ]
+};
+
+
+export default Bets;
---
+++ app/fixtures/s6e9-users.jsx
@@ -0,0 +1,68 @@
+const Users = {
+  'avclub-staffer-1': {
+    // displayName: 'Erik Adams',
+    wagers: {
+      'gameofthrones-6--': {
+        wager: ,
+        comment: "",
+      },
+
+    }
+  },
+  'avclub-staffer-2': {
+//    displayName: 'David Anthony',
+    wagers: {
+
+    }
+  },
+  'avclub-staffer-3': {
+//    displayName: 'Danette Chavez',
+    wagers: {
+
+    }
+  },
+  '0yomzuaZZRVJ1c4gzbL5M1ICWyf1': {
+//    displayName: 'Marah Eakin',
+    wagers: {
+
+    }
+  },
+  'avclub-staffer-5': {
+//    displayName: 'Josh Modell',
+    wagers: {
+
+    }
+  },
+  'avclub-staffer-6': {
+//    displayName: 'Caity PenzeyMoog',
+    wagers: {
+
+    }
+  },
+  'avclub-staffer-7': {
+//    displayName: 'Katie Rife',
+    wagers: {
+
+    }
+  },
+  'avclub-staffer-8': {
+//    displayName: 'John Teti',
+    wagers: {
+
+    }
+  },
+  'avclub-staffer-9': {
+//    displayName: 'Nick Wanserski',
+    wagers: {
+
+    }
+  },
+  'avclub-staffer-10': {
+//    displayName: 'Alex McCown',
+    wagers: {
+
+    }
+  }
+};
+
+export default Users;
