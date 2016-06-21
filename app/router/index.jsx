2a968439f02408a8a60af73de21641b34d5d780c
Craig Wright <craig@wright.name>
2016-06-20 20:01:27 -0700

    Profile page redirects when not logged in.

--- app/router/index.jsx
+++ app/router/index.jsx
@@ -8,12 +8,19 @@ import About from 'About';
 import Help from 'Help';
 
 
+var requireLogin = (nextState, replace, next) => {
+  if (!firebase.auth().currentUser) {
+    replace('/');
+  }
+  next();
+};
+
 export default (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={GameBoard}/>
-      <Route path="profile" component={ProfileBoard}/>
-      <Route path="profile/:userId" component={ProfileBoard}/>
+      <Route path="profile" component={ProfileBoard} onEnter={requireLogin}/>
+      <Route path="profile/:userId" component={ProfileBoard} onEnter={requireLogin}/>
       <Route path="help" component={Help}/>
       <Route path="about" component={About}/>
     </Route>
