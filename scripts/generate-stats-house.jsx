cbb2024052439672213f193f7e7efa34e973ef66
Craig Wright <craig@wright.name>
2017-08-03 21:35:43 -0700

    Updating scripts for Episode 4

--- scripts/generate-stats-house.jsx
+++ scripts/generate-stats-house.jsx
@@ -1,24 +1,17 @@
-import firebase from 'firebase';
-// App imports
-import {toCurrencyString} from '../app/utils';
-// import {processAllWagers, processOneUser} from './lib';
+/**
+ * generate-stats-house.jsx - Calculates the house's take / losses.
+ *
+ * SAFE TO RUN
+ */
+const seasonId = 'gameofthrones-06';
 
+import firebaseApp from './firebase-app';
+import moment from 'moment';
+import { toCurrencyString } from '../app/utils';
 
-// Initialize the app with a custom auth variable, limiting the server's access
-var config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket: process.env.STORAGE_BUCKET,
-  serviceAccount: process.env.FIREBASE_SERVICE_ACCOUNT_FILE || undefined,
-  databaseAuthVariableOverride: {
-    uid: "secret-service-worker"
-  }
-};
-firebase.initializeApp(config);
+// The app only has access as defined in the Security Rules
+const db = firebaseApp.database();
 
-
-console.log('Updating Firebase database', process.env.FIREBASE_DATABASE_URL);
 
 
 function processHouseStats(leaderboard) {
@@ -39,20 +32,13 @@ function processHouseStats(leaderboard) {
   return stats;
 }
 
-function errorFunc (err) {
-  console.log(err);
-}
 
-// The app only has access as defined in the Security Rules
-var db = firebase.database();
-var ref = db.ref();
-
-ref.child('leaderboard').once('value').then((snapshot) => {
+db.ref(`leaderboard/${seasonId}`).once('value').then((snapshot) => {
   let leaderboard = snapshot.val();
 
   try {
 
-    let stats = processHouseStats(leaderboard);
+    const stats = processHouseStats(leaderboard);
 
     console.log(`The house: balance: ${toCurrencyString(stats.balance)}, winnings: ${toCurrencyString(stats.winnings)}, losses: ${toCurrencyString(stats.losses)}`);
 
